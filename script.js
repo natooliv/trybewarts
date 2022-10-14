bf8c2e4dbdbcc686c03ef8f90e547d914bfbe14a
gabrieleferraz <gabriele.raz@hotmail.com>
2022-10-14 16:27:43 -0300

    Requisito 3 e 4

---
+++ script.js
@@ -0,0 +1,19 @@
+const email = document.getElementById('email')
+const senha = document.getElementById('senha')
+const botao = document.getElementById('btn-login')
+
+
+function login() {
+ 
+  if(email.value === "tryber@teste.com" && senha.value === "123456"){
+   alert ("Olá, Tryber!");
+  }else{
+   alert ("Email ou senha inválidos.")
+  }
+  botao.addEventListener('click',login)
+} 
+
+login ()
+
+
+
