8e36a24e815179c285cd53a7bd7e12219b8b420a
gabrieleferraz <gabriele.raz@hotmail.com>
2022-10-14 20:07:55 -0300

    ultimas

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('btn-login');
+const check = document.getElementById('agreement');
+const submit = document.getElementById('submit-btn');
+const contador = document.getElementById('counter');
+const box = document.getElementById('textarea');
 
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -12,3 +16,17 @@ function login() {
 }
 
 login();
+
+function verification() {
+  if (check.checked) {
+    submit.disabled = false;
+  }
+}
+
+check.addEventListener('click', verification);
+
+function contadorBox(event) {
+  contador.innerHTML = 500;
+  contador.innerHTML -= event.target.value.length;
+}
+box.addEventListener('keyup', contadorBox);
